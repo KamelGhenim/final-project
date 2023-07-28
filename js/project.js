c2ae8850d7569ce4e36d83a89be6de6cffc0eb68
Kamel <ghenimkamel@yahoo.com>
2023-07-28 13:04:26 +0100

    js added

---
+++ js/project.js
@@ -0,0 +1,16 @@
+const change = src => {
+    document.getElementById('mainimg').src = src
+}
+
+// const loginForm = document.getElementById('loginForm');
+// loginForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const username = document.getElementById('username').value;
+//   const password = document.getElementById('password').value;
+//   if (username.trim() === '' || password.trim() === '') {
+//     alert('Please enter both username and password.');
+//   } else {
+//     alert('Login successful!');
+//   }
+// });
+
